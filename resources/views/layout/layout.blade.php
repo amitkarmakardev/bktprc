4f86a73c6444b034335ac2d90084604294a64bc5
Amit Karmakar <amitkarmakar.dev@gmail.com>
2017-06-10 11:16:28 +0000

    Initial commit Create Home functionality

---
+++ resources/views/layout/layout.blade.php
@@ -0,0 +1,9 @@
+@extends('app')
+
+@section('content')
+
+    @include('layout.navbar')
+
+    @yield('subcontent')
+
+@stop
---
+++ resources/views/layout/sidemenu.blade.php
@@ -0,0 +1,10 @@
+<div class="list-group side-menu">
+    <a href="#" class="list-group-item">Home</a>
+    <a href="#" class="list-group-item">Program Calendar</a>
+    <a href="#" class="list-group-item">Photo Gallery</a>
+    <a href="#" class="list-group-item">Games and Sports</a>
+    <a href="#" class="list-group-item">Library</a>
+    <a href="#" class="list-group-item">Tourism</a>
+    <a href="#" class="list-group-item">Rural and Industrial Fair</a>
+</div>
+
