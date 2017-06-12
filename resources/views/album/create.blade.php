4238583e3d5bdee3355ae600278c7217ad9d9323
Amit Karmakar <amitkarmakar.dev@gmail.com>
2017-06-12 16:55:04 +0530

    Add album views

---
+++ resources/views/album/create.blade.php
@@ -0,0 +1,29 @@
+@extends('layout.layout')
+
+@section('subcontent')
+<div class="row content">
+    <h4>Create an album</h4>
+    <hr>
+    <form method="post" action="" class="form form-horizontal">
+    <div class="form-group">
+        <label for="name" class="control-label col-sm-2">Name</label>
+        <div class="col-sm-8">
+            <input type="text" name="name" class="form-control"/>        
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="name" class="control-label col-sm-2">Description</label>
+        <div class="col-sm-8">
+            <textarea name="description" class="form-control" rows="5"></textarea>        
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-sm-2"></div>
+        <div class="col-sm-8">
+            <input style="margin-left: 10px" type="reset" value="Reset" class="btn btn-default pull-right"></input>
+            <input type="submit" value="Submit"class="btn btn-primary pull-right"></input>
+        </div>
+    </div>
+    </form>
+</div>
+@stop
---
+++ resources/views/album/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.layout')
+
+@section('subcontent')
+    <div class="row content">
+    <div class="col-md-12">
+        <h4>Album Title <small class="pull-right"><a href="">add photos</a></small></h4>
+        <hr>
+    </div>
+
+    </div>
+@stop
