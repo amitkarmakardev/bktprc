2cadb4b82e2c1ee46bed3c846764ece47c634f3c
Amit Karmakar <amitkarmakar.dev@gmail.com>
2017-06-12 16:53:40 +0530

    Add index and create method to album controller

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -7,5 +7,17 @@ use App\Http\Controllers\Controller;
 
 class AlbumController extends Controller
 {
+
+    public function index(){
+        return view('album.index');
+    }
    
+   public function create(){
+       return view('album.create');
+   }
+
+   public function save(Request $request){
+       Album::create($request->all());
+       return redirect()->to()
+   }
 }
