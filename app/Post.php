ba18decd2cc184cf11f2ae3611ea6d0a7e25ea3b
Amit Karmakar <amitkarmakar.dev@gmail.com>
2017-07-14 16:55:21 +0000

    Add album function to post

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ['title', 'body', 'album_id'];
+
+    public function album(){
+        return $this->hasOne('App\Album', 'id', 'album_id');
+    }
 }
