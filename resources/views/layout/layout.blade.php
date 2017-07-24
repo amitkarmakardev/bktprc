ac930072e43bb99949713055bd74c6222d4204c5
Amit Karmakar <amitkarmakar.dev@gmail.com>
2017-07-24 10:43:52 +0530

    Banner text fix css

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -2,12 +2,9 @@
 
 @section('content')
     @include('layout.navbar')
-    {{--<img src="{{ asset('images/header.jpg') }}" alt="Header" style="width: 100%; height: 400px; margin-top: 10px; opacity: 0.8">--}}
     <div id="banner">
-        <div class="container">
-            <div id="banner-text">
-                <h1 style="color: white; font-size: 4.5em; font-family: Bitter, Kalpurush, serif">#Chagoler bachha</h1>
-            </div>
+        <div id="banner-text">
+            <h1>#GetItDone</h1>
         </div>
     </div>
     <div class="container main-container">
