ccae8143177e606b52eafb44048523b48a83b8f3
Amit Karmakar <amitkarmakar.dev@gmail.com>
2017-07-24 16:27:02 +0530

    Chagne header and body text

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     {{-- Fonts --}}
-    <link href="https://fonts.googleapis.com/css?family=Lato|Montserrat|Slabo+27px|Bitter" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Lato|Montserrat|Roboto|Bitter" rel="stylesheet">
     {{-- Font Awesome --}}
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
