f044a919e15ab2e72b0bdbb35a57260533f5c417
Amit Karmakar <amitkarmakar.dev@gmail.com>
2017-07-25 17:05:35 +0530

    Add links and font-awesome icons

--- resources/views/layout/sidemenu.blade.php
+++ resources/views/layout/sidemenu.blade.php
@@ -1,9 +1,9 @@
 <div class="list-group side-menu">
-    <a href="{{ url('') }}" class="list-group-item">Home</a>
-    <a href="{{ url('') }}" class="list-group-item">Life at Township</a>
-    <a href="{{ url('') }}" class="list-group-item">Recreation Club</a>
-    <a href="{{ url('') }}" class="list-group-item">Officers' Club</a>
-    <a href="#" class="list-group-item">Games and Sports</a>
-    <a href="{{ url('album') }}" class="list-group-item">Photo Albums</a>
-    <a href="#" class="list-group-item">Program Calendar</a>
+    <a href="{{ url('') }}" class="list-group-item"><i class="fa fa-home"></i> &nbsp;Home</a>
+    <a href="{{ url('life-at-township') }}" class="list-group-item"><i class="fa fa-group"></i> &nbsp;Life at Township</a>
+    <a href="{{ url('') }}" class="list-group-item"><i class="fa fa-gift"></i> &nbsp;Recreation Club</a>
+    <a href="{{ url('') }}" class="list-group-item"><i class="fa fa-shopping-bag"></i> &nbsp;Officers' Club</a>
+    <a href="#" class="list-group-item"><i class="fa fa-futbol-o"></i> &nbsp;Games and Sports</a>
+    <a href="{{ url('album') }}" class="list-group-item"><i class="fa fa-photo"></i> &nbsp;Photo Albums</a>
+    <a href="#" class="list-group-item"><i class="fa fa-calendar"></i> &nbsp;Program Calendar</a>
 </div>
